5308184a70394d9c8ac44fb8cf81bcd0165d2eae
Mark Valyavka <v221101mark@gmail.com>
2020-12-05 04:47:24 +0200

    codestyle fixes

--- src/main/java/ua/edu/ucu/tries/RWayTrie.java
+++ src/main/java/ua/edu/ucu/tries/RWayTrie.java
@@ -5,24 +5,30 @@ import ua.edu.ucu.collections.MyQueue;
 // Implementation taken from Algorithms 4th edition by R.Sedgewick
 public class RWayTrie implements Trie {
 
-    private static int R = 256;
+    private static final int R = 256;
     private Node root = new Node();
     private int size = 0;
 
     private static class Node {
         private Object val;
-        private Node[] next = new Node[R];
+        private final Node[] next = new Node[R];
     }
 
     public Object get(String key) {
         Node x = get(root, key, 0);
-        if (x == null) return null;
+        if (x == null) {
+            return null;
+        }
         return x.val;
     }
 
     private Node get(Node x, String key, int d) {
-        if (x == null) return null;
-        if (d == key.length()) return x;
+        if (x == null) {
+            return null;
+        }
+        if (d == key.length()) {
+            return x;
+        }
         char c = key.charAt(d);
         return get(x.next[c], key, d + 1);
     }
@@ -32,7 +38,9 @@ public class RWayTrie implements Trie {
     }
 
     private Node put(Node x, String key, Object val, int d) {
-        if (x == null) x = new Node();
+        if (x == null) {
+            x = new Node();
+        }
         if (d == key.length()) {
             x.val = val;
             return x;
@@ -44,23 +52,33 @@ public class RWayTrie implements Trie {
 
     private void collect(Node x, String pre,
                          MyQueue q) {
-        if (x == null) return;
-        if (x.val != null) q.enqueue(pre);
+        if (x == null) {
+            return;
+        }
+        if (x.val != null) {
+            q.enqueue(pre);
+        }
         for (char c = 0; c < R; c++)
             collect(x.next[c], pre + c, q);
     }
 
     private Node delete(Node x, String key, int d) {
-        if (x == null) return null;
+        if (x == null) {
+            return null;
+        }
         if (d == key.length())
             x.val = null;
         else {
             char c = key.charAt(d);
             x.next[c] = delete(x.next[c], key, d + 1);
         }
-        if (x.val != null) return x;
+        if (x.val != null) {
+            return x;
+        }
         for (char c = 0; c < R; c++)
-            if (x.next[c] != null) return x;
+            if (x.next[c] != null) {
+                return x;
+            }
         return null;
     }
 
